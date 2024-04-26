628237401424ccef4d724988584b8811b8b1dfc2
Shijin Zhang <1364226740@qq.com>
2024-04-26 23:37:31 +0800

    Change-source email address and add new test images.

--- env.ts
+++ env.ts
@@ -1,3 +1,3 @@
 export const SES_REGION = 'eu-west-1';
-export const SES_EMAIL_FROM = '1364226740@qq.com' ;
+export const SES_EMAIL_FROM = 'zsj1364226740@gmail.com' ;
 export const SES_EMAIL_TO =  '20104636@mail.wit.ie';
