20e4543821a1e529de56caa31b453ed77a3343b1
Shijin Zhang <1364226740@qq.com>
2024-04-26 23:41:26 +0800

    Improve-confirm mailer lambda function.

--- lambdas/confirmMailer.ts
+++ lambdas/confirmMailer.ts
@@ -24,8 +24,7 @@ const client = new SESClient({ region: SES_REGION});
 export const handler: SQSHandler = async (event: any) => {
     console.log("Event ", JSON.stringify(event));
     for (const record of event.Records) {
-        const recordBody = JSON.parse(record.body);
-        const snsMessage = JSON.parse(recordBody.Message);
+        const snsMessage = JSON.parse(record.Sns.Message);
 
         if (snsMessage.Records) {
             console.log("Record body ", JSON.stringify(snsMessage));
@@ -44,7 +43,6 @@ export const handler: SQSHandler = async (event: any) => {
                     await client.send(new SendEmailCommand(params));
                 } catch (error: unknown) {
                     console.log("ERROR is: ", error);
-                    // return;
                 }
             }
         }
@@ -62,10 +60,6 @@ function sendEmailParams({ name, email, message }: ContactDetails) {
                     Charset: "UTF-8",
                     Data: getHtmlContent({ name, email, message }),
                 },
-                // Text: {.           // For demo purposes
-                //   Charset: "UTF-8",
-                //   Data: getTextContent({ name, email, message }),
-                // },
             },
             Subject: {
                 Charset: "UTF-8",
@@ -92,13 +86,3 @@ function getHtmlContent({ name, email, message }: ContactDetails) {
   `;
 }
 
-// For demo purposes - not used here.
-function getTextContent({ name, email, message }: ContactDetails) {
-    return `
-    Received an Email. 📬
-    Sent from:
-        👤 ${name}
-        ✉️ ${email}
-    ${message}
-  `;
-}
